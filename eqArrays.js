9fb763ccd8faa8c83c4c2fe05fddf08dea80a5fe
ASA Dev <asadvlpr@gmail.com>
2021-11-01 16:09:53 -0400

    eqArrays refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,18 +1,13 @@
-const assertEqual = function(actual, expected) { 
-  if(actual === expected) {
-    console.log(`✅✅✅ Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
-  }
- }
+const assertEqual = require('./assertEqual');
 
- const eqArrays = function(array1, array2) {
-   for(let i = 0; i < array1.length; i++) {
-    if(array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true; 
+const eqArrays = function(array1, array2) {
+  for(let i = 0; i < array1.length; i++) {
+   if(array1[i] !== array2[i]) {
+     return false;
+   }
+ }
+ return true; 
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+module.exports = eqArrays;
+
