3629e52ead25729d590d8d00fde44e7d2c42230b
ASA Dev <asadvlpr@gmail.com>
2021-11-01 21:44:42 -0400

    mocha & chai refractored

--- test/testTail.js
+++ test/testTail.js
@@ -1,14 +1,22 @@
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const assert = require('chai').assert;
 
-// Test Case 1: Check the returned array elements HEYYYY ASHA SAGAL ZAK STEFAN
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
+describe("#tail", () => {
+    it("returns 1 for [1, 2, 3]", () => {
+      assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+    }); 
+  });
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+// // Test Case 1: Check the returned array elements HEYYYY ASHA SAGAL ZAK STEFAN
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs");
 
 
 
