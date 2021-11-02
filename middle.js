eaa2b083fc2609217f060b98ef107d41bcf2f4c5
ASA Dev <asadvlpr@gmail.com>
2021-11-01 20:36:40 -0400

    added console.log & refactored

--- middle.js
+++ middle.js
@@ -1,39 +1,28 @@
-const eqArrays = function(array1, array2) {
-  for (let i = 0; x < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
+const eqArrays = require('./eqArrays');
+const assertArrayEqual = require('./assertArrayEqual');
+
+const middle = function (array1) {
+  let middleArray = [];
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1.length <= 2) {
+      return middleArray;
+    } else if (array1.length % 2 === 1) {
+      let middleNum = array1[Math.floor(array1.length / 2)];
+      middleArray.push(middleNum);
+      return middleArray;
+    } else if (array1.length % 2 === 0) {
+      let upperMiddleNum = array1[Math.floor((array1.length - 1) / 2)];
+      let lowerMiddleNum = array1[Math.floor((array1.length + 1) / 2)];
+      middleArray.push(upperMiddleNum);
+      middleArray.push(lowerMiddleNum);
+      return middleArray;
     }
   }
-  return true;
+  //console.log(middleArray);
+  return middleArray;
 };
 
-  
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅ Assertion Passed: ${actual}  === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+module.exports = middle;
 
 
-const middle = function(array1, array2) {
-  let middleArray = []
-  let middleNum = array1[Math.floor(array1.length / 2)];
-  let upperMiddleNum = array1[Math.floor((array1.length - 1) / 2)];
-  let lowerMiddleNum =  array1[Math.floor((array1.length + 1) / 2)];
-  for(let i = 0; i < array1.length; i++) {
-    if(array1.length <= 2) {
-    return middleArray;
-  } else if(array1.length % 2 === 1) {
-    middleArray.push(middleNum);
-    return middleArray;
-  } else if (array1.length % 2 === 0) {
-    middleArray.push(upperMiddleNum);
-    middleArray.push(lowerMiddleNum);
-    return middleArray;
-  }
-}
-console.log(middleArray)
-return middleArray 
-}
