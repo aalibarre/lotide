9511dd5eb0853066ea44659fd5470bc7b258db34
ASA Dev <asadvlpr@gmail.com>
2021-11-01 20:35:30 -0400

    hey

---
+++ test/middleTest.js
@@ -0,0 +1,9 @@
+const eqArrays = require('../eqArrays');
+const assertArrayEqual = require('../assertArrayEqual');
+const middle = require('../middle');
+
+
+middle([1]); // => []
+middle([1, 2]);
+console.log(middle([1, 4,7,6]));
+
